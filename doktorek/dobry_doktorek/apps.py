6e8ce42bbfcd23c7eab81f887aab00cc757a7f08
Oskar Topolewicz <oskar@Oskars-MacBook-Air.local>
2024-10-28 11:43:21 +0100

    dobry doktorek

---
+++ doktorek/dobry_doktorek/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DobryDoktorekConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dobry_doktorek'
---
+++ doktorek/dobry_doktorek/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Wizyta, Pacjent
+
+def main(request):
+    WIZYTY = Wizyta.objects.count()
+    PACJENCI = Pacjent.objects.all()
+    return render(request, 'index.html', {'ilosc_wizyt': WIZYTY, 'pacjenci': PACJENCI})
+
